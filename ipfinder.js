e72091a50838922f1b8437830ebe396a8d21080e
snowkiddy <46402590+snowkiddy@users.noreply.github.com>
2019-10-23 04:34:58 +0530

    Create ipfinder.js

---
+++ ipfinder.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+                $.getJSON('https://ipinfo.io/json', function(data) {
+
+                        console.log(JSON.stringify(data, null, 2));
+
+                });
+
+        </script>
+
